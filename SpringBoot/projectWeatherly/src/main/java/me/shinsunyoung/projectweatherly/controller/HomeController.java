bab7fb80a10974dba0a6eff62cfad221ddb5fb98
DESKTOP-S2DD14G\it <rabbitharu1020@gmail.com>
2026-01-09 11:04:29 +0900

    초기 index.html, main.css 및 메인 홈 컨트롤러 (build.gradle) -2026-01-09

---
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/controller/HomeController.java
@@ -0,0 +1,13 @@
+package me.shinsunyoung.projectweatherly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/index.html"})
+    public String home() {
+        return "index"; // templates/index.html을 반환
+    }
+}
---
+++ SpringBoot/projectWeatherly/src/test/java/me/shinsunyoung/projectweatherly/ProjectWeatherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shinsunyoung.projectweatherly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeatherlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
