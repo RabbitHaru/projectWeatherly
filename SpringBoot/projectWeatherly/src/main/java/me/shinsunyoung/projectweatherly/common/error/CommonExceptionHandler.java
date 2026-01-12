df47f93df062a610a618bf72d287e7a8d59cc360
DESKTOP-6MPHVM6\it <zeta2011@naver.com>
2026-01-12 12:38:22 +0900

    ApiResponse 내용추가(api 실패 확인) GlobalExceptionHandler 추가(전역 예외 처리)

--- SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/common/error/CommonExceptionHandler.java
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/common/error/CommonExceptionHandler.java
@@ -1,30 +1,27 @@
 package me.shinsunyoung.projectweatherly.common.error;
 
+import me.shinsunyoung.projectweatherly.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class CommonExceptionHandler {
 
-    // 잘못된 요청
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResponse handleIllegalArgument(IllegalArgumentException e) {
-        return new ErrorResponse(
-                "BAD_REQUEST",
-                e.getMessage(),
-                LocalDateTime.now()
-        );
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgument(
+            IllegalArgumentException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(e.getMessage(), "INVALID_REQUEST"));
     }
 
-    // 나중에 추가용 ( 예외 못 잡았을 때)
     @ExceptionHandler(Exception.class)
-    public ErrorResponse handleException(Exception e) {
-        return new ErrorResponse(
-                "INTERNAL_SERVER_ERROR",
-                "서버에 오류가 발생했습니다.",
-                LocalDateTime.now()
-        );
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+
+        return ResponseEntity
+                .internalServerError()
+                .body(ApiResponse.error("서버 내부 오류", "INTERNAL_SERVER_ERROR"));
     }
 }
