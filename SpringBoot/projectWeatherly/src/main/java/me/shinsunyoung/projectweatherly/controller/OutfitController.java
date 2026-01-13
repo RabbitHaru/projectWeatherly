a11b26cf779edd2d378dccc83ef1249c600baf36
Geno <zeta2011@naver.com>
2026-01-13 16:41:44 +0900

    OUTFIT 추가본(미완) - OUTFIT 컨트롤러 추가 - OUTFIT CSS 추가 - OUTFIT HTML 추가

---
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/controller/OutfitController.java
@@ -0,0 +1,18 @@
+package me.shinsunyoung.projectweatherly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class OutfitController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OutfitController.class);
+
+    @GetMapping("/outfit")
+    public String outfitPage() {
+        logger.info("옷차림 페이지 접속 요청");
+        return "outfit"; // templates/outfit.html을 반환
+    }
+}
