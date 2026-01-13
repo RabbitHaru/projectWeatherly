2428042e79833c62d7350a91e61ee11c6e8f08e3
Geno <zeta2011@naver.com>
2026-01-13 14:35:20 +0900

    Clothing 내용 추가본

--- SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/clothing/enums/TemperatureRange.java
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/clothing/enums/TemperatureRange.java
@@ -1,4 +1,13 @@
 package me.shinsunyoung.projectweatherly.clothing.enums;
 
-public class TemperatureRange {
+public enum TemperatureRange {
+
+    HOT,            // 28℃ 이상
+    WARM,           // 23~27℃
+    MILD_WARM,      // 20~22℃
+    MILD,           // 17~19℃
+    COOL,           // 12~16℃
+    COLD_COOL,      // 9~11℃
+    COLD,           // 5~8℃
+    FREEZING        // 4℃ 이하
 }
