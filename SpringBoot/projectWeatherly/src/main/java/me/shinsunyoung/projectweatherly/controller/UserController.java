9d03baaac85cdeac61aa47089e3dd6a38c6acd1b
maando <alswns3853@gmail.com>
2026-01-17 21:10:50 +0900

    아직 작업중

--- SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/controller/UserController.java
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/controller/UserController.java
@@ -1,17 +1,26 @@
 package me.shinsunyoung.projectweatherly.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserController {
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+
+    @ModelAttribute
+    public void addRequestUri(Model model, HttpServletRequest request) {
+        model.addAttribute("requestURI", request.getRequestURI());
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";  // 이제 requestURI가 자동으로 추가됨
+    }
+
     @GetMapping("/signup")
-    public String signup(){
-        return "signup";
+    public String signup() {
+        return "signup";  // 이제 requestURI가 자동으로 추가됨
     }
 }
-
