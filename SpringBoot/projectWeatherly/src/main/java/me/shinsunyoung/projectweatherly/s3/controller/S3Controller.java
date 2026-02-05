6298b3038d19617dc520339a8a6488589a09fb0f
Geno <zeta2011@naver.com>
2026-02-05 16:41:04 +0900

    2026-02-05 React

--- SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/s3/controller/S3Controller.java
+++ SpringBoot/projectWeatherly/src/main/java/me/shinsunyoung/projectweatherly/s3/controller/S3Controller.java
@@ -3,10 +3,7 @@ package me.shinsunyoung.projectweatherly.s3.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.projectweatherly.s3.service.S3Service;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -24,5 +21,15 @@ public class S3Controller{
             return ResponseEntity.badRequest().build();
         }
     }
+    @GetMapping("/download")
+    public ResponseEntity<String> download(
+            @RequestParam("fileName")String fileName){
+        return ResponseEntity.ok(s3Service.downloadImg(fileName));
+    }
 
+    @GetMapping("/cloud")
+    public ResponseEntity<String> cloud(
+            @RequestParam("fileName") String fileName){
+        return ResponseEntity.ok(s3Service.getCloudFrontUrl(fileName));
+    }
 }
